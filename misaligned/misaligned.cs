3d4689de70c3f1af327ed4a3db87a79a689b4344
navanee867 <navanee777@gmail.com>
2024-08-07 19:17:53 +0530

    Update misaligned.cs

--- misaligned/misaligned.cs
+++ misaligned/misaligned.cs
@@ -1,22 +1,30 @@
 using System;
+
 using System.Diagnostics;
 
-namespace MisalignedSpace {
-    class Misaligned {
-        static int printColorMap() {
-            string[] majorColors = {"White", "Red", "Black", "Yellow", "Violet"};
-            string[] minorColors = {"Blue", "Orange", "Green", "Brown", "Slate"};
-            int i = 0, j = 0;
-            for(i = 0; i < 5; i++) {
-                for(j = 0; j < 5; j++) {
-                    Console.WriteLine("{0} | {1} | {2}", i * 5 + j, majorColors[i], minorColors[i]);
+namespace MisalignedSpace
+{
+    class Misaligned
+    {
+        private static readonly string[] _majorColor = { "White", "Red", "Black", "Yellow", "Violet" }; 
+        private static readonly string[] _minorColor = { "Blue", "Orange", "Green", "Brown", "Slate" }; 
+
+        private static void PrintColorMap()
+        {
+            for (int majorColorIndex = 0; majorColorIndex < _majorColor.Length; majorColorIndex++)
+            {
+                for (int minorColorIndex = 0; minorColorIndex < _minorColor.Length; minorColorIndex++)
+                {
+                    Console.WriteLine($"{((majorColorIndex * 5 + minorColorIndex) + 1),-3} | {_majorColor[majorColorIndex],-7} | {_minorColor[minorColorIndex]}");
                 }
             }
-            return i * j;
         }
-        static void Main(string[] args) {
-            int result = printColorMap();
-            Debug.Assert(result == 25);
+
+        private static void Main(string[] args)
+        {
+            PrintColorMap();
+
+            Debug.Assert(_majorColor.Length == 7);
             Console.WriteLine("All is well (maybe!)");
         }
     }
