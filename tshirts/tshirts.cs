34cd1ad86ca4f19e68bba0c6ed24c8f3a6ff5ccd
navanee867 <navanee777@gmail.com>
2024-08-07 19:27:46 +0530

    Update tshirts.cs

--- tshirts/tshirts.cs
+++ tshirts/tshirts.cs
@@ -14,7 +14,9 @@ namespace TshirtSpace {
         }
         static void Main(string[] args) {
             Debug.Assert(Size(37) == "S");
+            Debug.Assert(Size(38) == 'S');
             Debug.Assert(Size(40) == "M");
+            Debug.Assert(Size(42) == 'L');
             Debug.Assert(Size(43) == "L");
             Console.WriteLine( "All is well (maybe!)");
         }
