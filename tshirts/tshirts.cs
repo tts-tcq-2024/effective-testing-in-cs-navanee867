5497f255c3fe3435eb5b1ecacdf28eb02ed4bf35
navanee867 <navanee777@gmail.com>
2024-08-08 11:56:07 +0530

    Update tshirts.cs

--- tshirts/tshirts.cs
+++ tshirts/tshirts.cs
@@ -1,34 +1,33 @@
 using System;
 using System.Diagnostics;
 
-namespace TshirtSpace {
-    public static char TshirtSize(int cms)
-    {
-        char sizeName = '';
-
-        if (cms < 38)
-        {
-            sizeName = 'S';
-        }
-        else if (cms > 38 && cms < 42)
-        {
-            sizeName = 'M';
-        }
-        else if (cms > 42)
-        {
-            sizeName = 'L';
-        }
-
-        return sizeName;
-    }
-
-    public static void Main()
+namespace TshirtSpace
 {
-    Debug.Assert(TshirtSize(37) == 'S');
-    Debug.Assert(TshirtSize(38) == 'S');
-    Debug.Assert(TshirtSize(40) == 'M');
-    Debug.Assert(TshirtSize(42) == 'L');
-    Debug.Assert(TshirtSize(44) == 'L');
-    Console.WriteLine("All is well (maybe!)");
-}
+    class Tshirt
+    {
+        static string Size(int cms)
+        {
+            if (cms < 38)
+            {
+                return "S";
+            }
+            else if (cms > 38 && cms < 42)
+            {
+                return "M";
+            }
+            else
+            {
+                return "L";
+            }
+        }
+        static void Main(string[] args)
+        {
+            Debug.Assert(Size(37) == "S");
+            Debug.Assert(Size(38) == "S");
+            Debug.Assert(Size(42) == "L");
+            Debug.Assert(Size(40) == "M");
+            Debug.Assert(Size(43) == "L");
+            Console.WriteLine("All is well (maybe!)");
+        }
+    }
 }
